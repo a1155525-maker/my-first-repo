e427ed17bdc08292900a89d86e2efd94d8f0ca2b
a1155525-maker <a1155525@mail.nuk.edu.tw>
2026-09-16 17:31:47 +0800

    first try

---
+++ test1/Untitled-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    printf("Hello, CSIE!\n");
+    printf("請輸入你的名字: ");
+    if (scanf("%s", name) == 1) {
+        printf("歡迎, %s!\n", name);
+    }
+    return 0;
+}
